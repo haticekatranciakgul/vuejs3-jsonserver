fe4a5af53a8fb9cf37f722c5f62044f4f9670616
hatice <hatice@hotmail.com>
2022-08-26 01:01:59 +0300

    cmm

--- hello-axios/src/App.vue
+++ hello-axios/src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <di>
+    <h1>Todos</h1>
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{ todo.name }}
+      </li>
+    </ul>
+  </di>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "TodoList",
+  data() {
+    return {
+      todos: []
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(`http://localhost:3001/todos`);
+
+      this.todos = res.data;
+    } catch (e) {
+      console.error(e);
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h1 {
+  text-decoration: underline;
+}
+
+li {
+  color: white;
 }
 </style>
